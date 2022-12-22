dc02611f9ff51424b5b966580da9306a435009c5
gouxinyu <gxy880520@qq.com>
2022-12-23 00:43:29 +0800

    change img

--- components/Slide.vue
+++ components/Slide.vue
@@ -42,7 +42,7 @@ export default defineComponent({
     display: flex;
     align-items: center;
     justify-content: center;
-    background: url("/img/slidebg.png") no-repeat;
+    background: url("/img/bg.png") no-repeat;
     background-size: cover;
 }
 
